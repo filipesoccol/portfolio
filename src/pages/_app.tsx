707bfd26458454d88f63e19e69c45f4d8044610d
Filipe Soccol <filipe.soccol@gmail.com>
2023-06-22 11:02:14 -0300

    Addded 3D effect to header.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
+import '@/styles/globals.scss'
+import { Quicksand } from 'next/font/google'
 import type { AppProps } from 'next/app'
 
+// If loading a variable font, you don't need to specify the font weight
+const quicksand = Quicksand({ weight: ['300', '400'], subsets: ['latin'] })
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={quicksand.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
