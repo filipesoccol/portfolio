19e8ffca298a16b67f0986a1316136d31f9f171d
Filipe Soccol <filipe.soccol@gmail.com>
2023-09-06 22:10:14 -0300

    Closer to figma design.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,15 @@
 import '@/styles/globals.scss'
-import { Quicksand } from 'next/font/google'
+import { Zen_Antique, Space_Mono } from 'next/font/google'
 import type { AppProps } from 'next/app'
+import { Html } from 'next/document'
 
 // If loading a variable font, you don't need to specify the font weight
-const quicksand = Quicksand({ weight: ['300', '400'], subsets: ['latin'] })
+const zen = Zen_Antique({ weight: ['400'], subsets: ['latin'], variable: '--zen-font' })
+const space = Space_Mono({ weight: ['400'], subsets: ['latin'], variable: '--space-font' })
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={quicksand.className}>
+    <main className={`${zen.variable} ${space.variable}`}>
       <Component {...pageProps} />
     </main>
   )
